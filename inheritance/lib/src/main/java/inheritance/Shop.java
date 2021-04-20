e7bbf0979b63f6bb7faf4ee43a0e0f18466efe29
semo4 <fotboy1788@hotmail.com>
2021-04-20 11:55:10 +0300

    correct lab 6

--- inheritance/lib/src/main/java/inheritance/Shop.java
+++ inheritance/lib/src/main/java/inheritance/Shop.java
@@ -4,35 +4,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Shop {
-    String name;
-    String description;
-    int numberOfDollar;
-    int numberStars;
-    Review r;
-    List<Integer> stars = new ArrayList<>();
 
-    public Shop(String name, String description, int numberOfDollar) {
+    public String name;
+    public String price;
+    public int star;
+    ArrayList<Review> reviews = new ArrayList<>();
+
+
+    public Shop(String name, String price){
         this.name = name;
-        this.description = description;
-        this.numberOfDollar = numberOfDollar;
-    }
-
-    public void addReview(String body, String author, int numberOfStars){
-        stars.add(numberOfStars);
-        r = new Review(this.name,body,author,numberOfStars);
-        updateStars();
-    }
-
-    public void updateStars(){
-        int total = 0;
-        for (int i = 0; i < stars.size(); i++) {
-            total = total + stars.get(i);
-        }
-        total = total/ stars.size();
-        this.numberStars = total;
+        this.price = price;
+        this.star = 0;
     }
 
     public String toString(){
-        return this.name+" Shop name "+ "Shop Info "+ this.description + " "+ this.numberOfDollar +" Rating "+numberStars;
+        return "Name: " + this.name + ", Price: " + this.price + ", Rating: " + this.star;
+    }
+
+    public void addReview(Review potato){
+        reviews.add(potato);
+        potato.shop = this;
+
+        updateStars();
+    }
+
+    private void updateStars(){
+        //create variable to store sum
+        //loop over reviews and add stars
+        //divide sum by number of reviews
+        //update star
+        int total = 0;
+        for (int i = 0; i < reviews.size(); i++){
+            total += reviews.get(i).stars;
+        }
+        total /= reviews.size();
+        this.star = total;
     }
 }
